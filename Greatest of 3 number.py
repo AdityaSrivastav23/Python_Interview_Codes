d54e372bed835f6e572a0bf91f5b1467cf609ce8
Aditya_Srivastav <77807589+AdityaSrivastav23@users.noreply.github.com>
2022-03-27 10:55:42 +0530

    Add files via upload

---
+++ Greatest of 3 number.py
@@ -0,0 +1,9 @@
+n1 = int(input("please give first number n1: "))
+n2 = int(input("please give second number n2: "))
+n3 = int(input("please give third number n3: "))
+if n1>=n2 and n1>=n3: 
+	print(f"n1= {n1} is greatest among n2= {n2} and n3= {n3}");
+if n2>=n1 and n2>=n3:
+	print(f"n2= {n2} is greatest among n1= {n1} and n3= {n3}");
+if n3>=n1 and n3>=n2:
+	print(f"n3= {n3} is greatest among n1= {n1} and n2= {n2}"); 
